80d59348102dcaceb4b5b758dfbec71f7ee3735a
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-30 22:29:10 +0700

    Initial Commit

---
+++ LogMealSwiftData/LogMealSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogMealSwiftDataApp.swift
+//  LogMealSwiftData
+//
+//  Created by Michelle Angela Aryanto on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LogMealSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LogMealSwiftDataTests/LogMealSwiftDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogMealSwiftDataTests.swift
+//  LogMealSwiftDataTests
+//
+//  Created by Michelle Angela Aryanto on 30/10/24.
+//
+
+import Testing
+@testable import LogMealSwiftData
+
+struct LogMealSwiftDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
